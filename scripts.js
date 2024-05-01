3f9a37ab02ac327c67ed2d0e70946933d32de44e
Mariona <151879084+mariona78@users.noreply.github.com>
2024-05-01 12:52:25 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,22 @@
+{
+    "workbench.colorTheme": "Default Dark Modern",
+    "redhat.telemetry.enabled": true,
+    "java.home": "/Applications/Visual Studio Code.app/Contents/Resources/appjdk-17",
+    "editor.defaultFoldingRangeProvider": "redhat.java",
+    "maven.terminal.useJavaHome": true
+}
+"java.configuration.runtime": [
+    {
+      "name": "JavaSE-1.8",
+      "path": "/path/to/jdk-8",
+    },
+    {
+      "name": "JavaSE-11",
+      "path": "/path/to/jdk-11",
+    },
+    {
+      "name": "JavaSE-20",
+      "path": "/path/to/jdk-20",
+      "default": true
+    },
+  ]
